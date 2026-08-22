c6a856667468da5a59b1da61fe7bb06336734b43
Ben <ben@nixos.localdomain>
2026-08-22 20:31:32 +0000

    m

--- .config/nvim/lua/plugins/mason-lsp.lua
+++ .config/nvim/lua/plugins/mason-lsp.lua
@@ -4,12 +4,7 @@ return {
     ensure_installed = {
         "lua_ls",
         "rust_analyzer",
-        "clangd",
         "bashls",
-        "gopls",
-        "jdtls",
-        "pyright",
-        "tinymist",
     },
         handlers = {
             tinymist = function()
